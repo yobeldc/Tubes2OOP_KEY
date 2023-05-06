482055cf2ce7134db8a9afc23e93504c9a450fda
Akhmad Setiawan <13521164@std.stei.itb.ac.id>
2023-05-07 00:30:15 +0700

    add icon to updateMember

--- OOP_2/src/main/java/org/openjfx/UpdateMember/UpdateMemberPage.java
+++ OOP_2/src/main/java/org/openjfx/UpdateMember/UpdateMemberPage.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -18,22 +20,68 @@ import javafx.geometry.Pos;
 public class UpdateMemberPage extends VBox {
 
     public UpdateMemberPage() {
+        VBox finalBox = new VBox();
+        finalBox.getStyleClass().addAll("page3");
+        finalBox.setPrefSize(1000, 800);
+        finalBox.setMaxWidth(1000);
+        finalBox.setMaxHeight(800);
+        finalBox.setAlignment(Pos.CENTER);
+
         VBox UpdateMemberPage = new VBox();
         UpdateMemberPage.setPadding(new Insets(10));
         UpdateMemberPage.setSpacing(5);
         UpdateMemberPage.getStyleClass().addAll("page");
-
+        
         // Create a new box
-        VBox textBox = new VBox();
-        textBox.getStyleClass().addAll("box");
-        textBox.setPrefSize(416, 529);
-        textBox.setMaxWidth(416);
-        textBox.setMaxHeight(529);
+        VBox searchBox = new VBox();
+        searchBox.getStyleClass().addAll("box");
+        searchBox.setPrefSize(900, 50);
+        searchBox.setMaxWidth(900);
+        searchBox.setMaxHeight(50);
+        VBox.setMargin(searchBox, new Insets(0, 0, 10, 0));
+        
+        HBox search = new HBox();
+        search.setAlignment(Pos.CENTER_LEFT);
+        search.getStyleClass().addAll("box");
+        search.setPrefSize(471, 47);
+        search.setMaxWidth(471);
+        search.setMaxHeight(47);
+
+        // Create the icon
+        Button searchBtn = new Button();
+        searchBtn.setPrefSize(30, 30);
+        searchBtn.setMaxWidth(30);
+        searchBtn.setMaxHeight(30);
+        ImageView icon = new ImageView(new Image(getClass().getResource("/search.png").toExternalForm()));
+        icon.setFitHeight(20);
+        icon.setFitWidth(20);
+        searchBtn.setGraphic(icon);
+        
+        // Create the text field
+        TextField memberField = new TextField();
+        memberField.setPromptText("Cari Nama Member");
+        memberField.setPrefSize(280, 30);
+        memberField.setMaxWidth(280);
+        memberField.setMaxHeight(30);
+        
+        // Add the icon and text field to the HBox
+        search.getChildren().addAll(searchBtn, memberField);
+        HBox.setMargin(searchBtn, new Insets(0, 0, 0, 75));
+        
+        searchBox.getChildren().addAll(search);
+        searchBox.setAlignment(Pos.CENTER);
+        
+        // Create a new box
+        VBox inputBox = new VBox();
+        inputBox.getStyleClass().addAll("box");
+        inputBox.setPrefSize(416, 529);
+        inputBox.setMaxWidth(416);
+        inputBox.setMaxHeight(529);
 
         // Add label as a title
-        Label updateLabel = new Label("Perbaharui Informasi Member");
+        Label updateLabel = new Label("Perbarui Informasi Member");
         VBox.setMargin(updateLabel, new Insets(50, 0, 0, 0));
-        textBox.setAlignment(Pos.TOP_CENTER);
+        inputBox.setAlignment(Pos.TOP_CENTER);
         
         // Add text field for Ubah Nama
         TextField nameField = new TextField();
@@ -87,18 +135,19 @@ public class UpdateMemberPage extends VBox {
             // }
             // );
 
-        textBox.getChildren().add(updateLabel);
-        textBox.getChildren().add(nameField);
-        textBox.getChildren().add(telephoneField);
-        textBox.getChildren().add(updateStatus);
-        textBox.getChildren().add(submit);
+        inputBox.getChildren().add(updateLabel);
+        inputBox.getChildren().add(nameField);
+        inputBox.getChildren().add(telephoneField);
+        inputBox.getChildren().add(updateStatus);
+        inputBox.getChildren().add(submit);
         submit.setPrefSize(335, 45);
         submit.setMaxWidth(335);
         submit.setMaxHeight(335);
         VBox.setMargin(submit, new Insets(50, 0, 0, 0));
 
         // Add the new box as a child of the UpdateMemberPage VBox
-        getChildren().add(textBox);
+        finalBox.getChildren().addAll(searchBox, inputBox);
+        getChildren().add(finalBox);
         setAlignment(Pos.CENTER);
     }
 }
